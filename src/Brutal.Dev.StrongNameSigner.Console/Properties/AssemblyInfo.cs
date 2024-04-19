0e6a72d26cd7ae472910d430b5789b9a35784555
Piero Viano <piero.viano@gmail.com>
2024-04-19 06:39:41 +0300

    ''

--- src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
+++ src/Brutal.Dev.StrongNameSigner.Console/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle(".NET Assembly Strong-Name Signer Console")]
-[assembly: AssemblyDescription("Console application to sign .NET assemblies with a strong-name key and fix assembly references.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://brutaldev.com")]
-[assembly: AssemblyProduct("Brutal.Dev.StrongNameSigner.Console")]
-[assembly: AssemblyCopyright("Copyright © 2013-2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -22,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("40a27050-4fa1-4033-a351-b794eb7fac93")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.1.0")]
-[assembly: AssemblyFileVersion("3.2.1.0")]
--- src/Brutal.Dev.StrongNameSigner.UI/Properties/AssemblyInfo.cs
+++ src/Brutal.Dev.StrongNameSigner.UI/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle(".NET Assembly Strong-Name Signer")]
-[assembly: AssemblyDescription("User interface to sign .NET assemblies with a strong-name key and fix assembly references.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://brutaldev.com")]
-[assembly: AssemblyProduct("Brutal.Dev.StrongNameSigner.UI")]
-[assembly: AssemblyCopyright("Copyright © 2013-2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -22,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("561e3d75-261a-4992-b331-25a104442fa2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.1.0")]
-[assembly: AssemblyFileVersion("3.2.1.0")]
